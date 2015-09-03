e85a6680fe001483a6102b0d94af767d501dc92a
theInternauts <cricketw@wearetheinternauts.com>
2015-09-03 00:52:02 -0700

    ISSUE-1 each line can be toggled to remain highlighted or not

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,56 +1,48 @@
-javascript: (function (window) {
-
-		function isRowTarget(node) {
-			// hardcoded to test for tr.route_row as the target node
-			// returns the found node or FALSE
-			var foundTarget = false;
-			if(node && node.nodeName == "TR") {
-				// Search for the CSS class!
-				if(Array.prototype.slice.call(node.classList).indexOf("route_row") >= 0) {
-					// Bingo!
-					console.log("route_row element clicked!");
-					foundTarget = true;
-					
-					// Now do something here...					
-					
-				}
+javascript: (function (window){
+	function isRowTarget(node){				
+		var elm = node;
+		var foundTarget = false;
+		if(elm !== undefined && elm !== null && elm.nodeName == "TR"){			
+			if(Array.prototype.slice.call(elm.classList).indexOf("route_row") >= 0){				
+				foundTarget = true;
 			}
-			return foundTarget ? node : false;
 		}
+		if(foundTarget){
+			return elm;
+		} else {
+			return false;
+		}
+	}
 
-		function isDelegatedTarget(node) {
-			// hardcoded to consider #test_root as the delegate
-			// returns the found node or FALSE
-			var isDelegate = (node && node.id == 'test_root');
-			window.result = isRowTarget(node);
-			var found = (isDelegate || result);
-			console.log('f: ', found, 'd: ', isDelegate, 'r: ', result);
-			if(!found && node) {
-				isDelegatedTarget(node.parentNode);				
+	function isDelegatedTarget(node){				
+		var isDelegate = (node !== null && node.id == 'test_root');
+		var result = isRowTarget(node);
+		var found = (isDelegate || result);				
+		if(!found && node){
+			return isDelegatedTarget(node.parentNode);				
+		} else {					
+			return result;
+		}
+	}
+
+	function toggleHighlight(evt){
+		evt.stopPropagation();
+		var targetClicked = isDelegatedTarget(evt.target);				
+		if(targetClicked){
+			if (targetClicked.classList.contains("frozen")){
+				targetClicked.classList.remove("frozen");
 			} else {
-				return result;
+				targetClicked.classList.add("frozen");
 			}
 		}
+	}
 
-		function toggleHighlight(evt) {
-			console.log("toggling....");
-			window.targetClicked = isDelegatedTarget(evt.target);
-			if(targetClicked) {
-				if (targetClicked.classList.contains("frozen")){
-					targetClicked.classList.remove("frozen");
-				} else {
-					targetClicked.classList.add("frozen");
-				}
-			}
-			console.log("toggle: ", targetClicked);
-		}
-
-		var style = document.createElement('style');
-		style.type = 'text/css';
-		style.innerHTML = 'tr.route_row:hover { background-color: #ddfcf7; } .frozen { background-color: #fcfadd; }';
-		document.getElementsByTagName('head')[0].appendChild(style);
-		document.addEventListener('click', toggleHighlight);
-}(window));
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = 'tr.route_row:hover { background-color: #ddfcf7; } .frozen { background-color: #fcfadd; }';
+	document.getElementsByTagName('head')[0].appendChild(style);
+	document.addEventListener('click', toggleHighlight);
+})(window);
 
 // http://code.tutsplus.com/tutorials/create-bookmarklets-the-right-way--net-18154
 // http://benalman.com/projects/run-jquery-code-bookmarklet/
